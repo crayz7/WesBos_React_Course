f8102c58d5680d6be050e4dcf5827d32a611e71d
Jon C <jchan178@gmail.com>
2018-09-11 11:36:35 -0500

    Update base.js

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,7 +2,7 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp ({
-        apiKey: "AIzaSyAXvsPFUI8em77vVjOE9DXLfgI3RtLw3rc",
+        apiKey: "",
         authDomain: "catch-of-the-day-jon-c.firebaseapp.com",
         databaseURL: "https://catch-of-the-day-jon-c.firebaseio.com",
       
